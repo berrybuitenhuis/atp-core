0d38475ba22c9c0518033798dc9445ec40f28d47
B.Buitenhuis <b.buitenhuis@bva-auctions.com>
2017-10-20 12:49:24 +0200

    Remove token-data (oauth-server) from options-factory (data from session)

--- src/Zf/Factory/OptionsFactory.php
+++ src/Zf/Factory/OptionsFactory.php
@@ -10,16 +10,7 @@ class OptionsFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
-        $config = $container->get('Config');
-        $storage = [];
-        foreach ($config['zf-oauth2']['storage'] as $storageKey => $storagesService) {
-            $storage[$storageKey] = $container->get($storagesService);
-        }
-        $oAuthServer = new OAuth2Server($storage);
-
-        $options = new $requestedName(
-            $oAuthServer
-        );
+        $options = new $requestedName();
 
         return $options;
     }
