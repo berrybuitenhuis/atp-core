f5d2f5c60b5297b51f59ce0818beb5e8e5a7ac90
B.Buitenhuis <b.buitenhuis@bva-auctions.com>
2018-04-26 11:19:20 +0200

    Add (form) input-filter password

---
+++ src/Zf/InputFilter/PasswordInputFilter.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace AtpCore\Zf\InputFilter;
+
+use Zend\InputFilter\InputFilter;
+
+class PasswordInputFilter
+{
+
+    /**
+     * Get InputFilter for a Password-type field
+     *
+     * @param $name
+     * @param boolean $required
+     * @param integer $minLength
+     * @return void|InputFilter
+     */
+    public function getFilter($name, $required = false, $minLength = 8, $minUppercase = 1, $minLowercase = 1, $minDigits = 1, $minSpecialCharacters = 1)
+    {
+        if ($name == null) {
+            return;
+        } else {
+            $filter = [
+                'name'      => $name,
+                'required'  => $required,
+                'filters'   => [
+                    ['name' => 'StripTags'],
+                    ['name' => 'StringTrim'],
+                ],
+                'validators' => [
+                    [
+                        'name' => 'StringLength',
+                        'options' => [
+                            'min' => $minLength,
+                            'max' => 250,
+                        ]
+                    ],[
+                        'name' => 'Regex',
+                        'options' => [
+                            'pattern' => '/[A-Z]{' . $minUppercase . ',}/',
+                        ]
+                    ],[
+                        'name' => 'Regex',
+                        'options' => [
+                            'pattern' => '/[a-z]{' . $minLowercase . ',}/',
+                        ]
+                    ],[
+                        'name' => 'Regex',
+                        'options' => [
+                            'pattern' => '/[0-9]{' . $minDigits . ',}/',
+                        ]
+                    ],[
+                        'name' => 'Regex',
+                        'options' => [
+                            'pattern' => '/[~!@#$%^&*_\-+=`|\(){}[\]:;"\'<>,.?\/\\]{' . $minSpecialCharacters . ',}/',
+                        ]
+                    ],
+                ],
+            ];
+
+            $inputFilter = new InputFilter();
+            $inputFilter->add($filter, $name);
+            return $inputFilter;
+        }
+    }
+
+}
