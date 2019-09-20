758a8913eed9da6d22b3e22eecd03b60156b7cdd
B. Buitenhuis <b.buitenhuis@bva-auctions.com>
2019-09-20 16:55:51 +0200

    Add format-class (phonenumber)

---
+++ src/Format.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AtpCore;
+
+class Format
+{
+    public function phoneNumber($countryCode, $nationalCode, $subscriberNumber)
+    {
+        // Skip if no phone-number available
+        if (empty($subscriberNumber)) return;
+
+        // Format phone-number
+        $phoneNumber = '';
+
+        // Add country-code to phone-number
+        if (is_numeric(str_replace('+', '', $countryCode))) {
+            $phoneNumber .= "+";
+        }
+        $phoneNumber .= trim(str_replace("00", "", str_replace('+', '', $countryCode)));
+
+        // Add national-code to phone-number
+        if (strlen($nationalCode) != 0 && is_numeric($nationalCode)) {
+            if (strlen($phoneNumber) > 0 && substr($nationalCode, 0, 1) == '0') {
+                $phoneNumber .= " (0)" . substr($nationalCode, 1, strlen($nationalCode) - 1);
+            } elseif (strlen($phoneNumber) == 0) {
+                $phoneNumber .= $nationalCode;
+            } else {
+                $phoneNumber .= " (0)" . $nationalCode;
+            }
+        }
+
+        // Add subscriber-number to phone-number
+        if (!empty(preg_replace("/\D/", "", $subscriberNumber)))
+            $phoneNumber .= '-' . $subscriberNumber;
+
+        // Return
+        return $phoneNumber;
+    }
+
+}
