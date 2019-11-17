813d63dcb4b1f232324452993e8020cae7be3868
B. Buitenhuis <b.buitenhuis@bva-auctions.com>
2019-11-17 15:04:55 +0100

    Add AWS CloudWatch class

---
+++ src/Api/Aws/CloudWatch.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * API-information: https://docs.aws.amazon.com/aws-sdk-php/v3/api/
+ */
+namespace AtpCore\Api\Aws;
+
+use AtpCore\BaseClass;
+use Aws\CloudWatch\CloudWatchClient;
+
+class CloudWatch extends BaseClass
+{
+
+    private $client;
+    private $config;
+
+    /**
+     * Constructor
+     *
+     * @param string $version
+     * @param string $region
+     * @param string $awsKey
+     * @param string $awsSecret
+     */
+    public function __construct($version = "latest", $region = "eu-west-1", $awsKey = null, $awsSecret = null)
+    {
+        // Set config
+        $this->config = [
+            'version' => $version,
+            'region' => $region,
+        ];
+        if (!empty($awsKey)) {
+            $this->config['credentials'] = [
+                'key' => $awsKey,
+                'secret' => $awsSecret,
+            ];
+        }
+
+        // Set client
+        $this->client = new CloudWatchClient($this->config);
+
+        // Reset error-messages
+        $this->resetErrors();
+    }
+
+    /**
+     * Get alarms in specific state (OK|ALARM|INSUFFICIENT_DATA)
+     * @param $stateName
+     * @return \Aws\Result|bool
+     */
+    public function describeAlarms($stateName)
+    {
+        try {
+            return $this->client->describeAlarms(["StateValue"=>$stateName]);
+        } catch (Exception $e) {
+            $this->setErrorData($e->getMessage());
+            return false;
+        }
+    }
+
+}
