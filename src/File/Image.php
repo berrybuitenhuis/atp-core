7bd0312046559b93125ba84bf27a8f1f0f19ce3c
B. Buitenhuis <b.buitenhuis@bva-auctions.com>
2020-07-28 10:53:14 +0200

    Improve image-resize function (chaining)

--- src/File/Image.php
+++ src/File/Image.php
@@ -2,106 +2,98 @@
 
 namespace AtpCore\File;
 
+use AtpCore\BaseClass;
 use \Gumlet\ImageResize;
 use \Spatie\ImageOptimizer\OptimizerChainFactory;
 
-class Image
+class Image extends BaseClass
 {
-    public $errorData = [];
-    public $messages = [];
-
     /**
      * Resize image by content
      *
      * @param string $content
      * @param int $maxHeight
      * @param int $maxWidth
-     * @return string|boolean
+     * @param int|null $thumbMaxHeight
+     * @param int|null $thumbMaxWidth
+     * @return array|boolean|string
      */
-    public function resizeByContent($content, $maxHeight, $maxWidth)
+    public function resizeByContent($content, $maxHeight, $maxWidth, $thumbMaxHeight = null, $thumbMaxWidth = null)
     {
-        try {
-            // Resize image
-            $image = ImageResize::createFromString($content);
-            $image->resizeToBestFit($maxWidth, $maxHeight);
-            $resizedImageString = $image->getImageAsString();
-
-            // Optimized image
-            return $this->optimize($resizedImageString);
-        } catch (\Throwable $e) {
-            $this->messages[] = "Unable to resize image";
-            $this->errorData[] = $e->getMessage();
-            return false;
-        }
+        return $this->resize(null, $content, $maxHeight, $maxWidth, $thumbMaxHeight, $thumbMaxWidth);
     }
 
     /**
      * Resize image by file
      *
-     * @param string $imagePath
+     * @param string $path
      * @param int $maxHeight
      * @param int $maxWidth
-     * @return string|boolean
+     * @param int|null $thumbMaxHeight
+     * @param int|null $thumbMaxWidth
+     * @return array|boolean|string
      */
-    public function resizeByFile($imagePath, $maxHeight, $maxWidth)
+    public function resizeByFile($path, $maxHeight, $maxWidth, $thumbMaxHeight = null, $thumbMaxWidth = null)
     {
-        try {
-            // Resize image
-            $image = new ImageResize($imagePath);
-            $image->resizeToBestFit($maxWidth, $maxHeight);
-            $resizedImageString = $image->getImageAsString();
-
-            // Optimized image
-            return $this->optimize($resizedImageString);
-        } catch (\Throwable $e) {
-            $this->messages[] = "Unable to resize image";
-            $this->errorData[] = $e->getMessage();
-            return false;
-        }
+        return $this->resize($path, null, $maxHeight, $maxWidth, $thumbMaxHeight, $thumbMaxWidth);
     }
 
     /**
      * Resize image by URL
      *
-     * @param string $imageUrl
+     * @param string $url
      * @param int $maxHeight
      * @param int $maxWidth
-     * @return string|boolean
+     * @param int|null $thumbMaxHeight
+     * @param int|null $thumbMaxWidth
+     * @return array|boolean|string
      */
-    public function resizeByUrl($imageUrl, $maxHeight, $maxWidth)
+    public function resizeByUrl($url, $maxHeight, $maxWidth, $thumbMaxHeight = null, $thumbMaxWidth = null)
     {
         // Check status-code of URL
-        $validUrl = $this->validateUrl($imageUrl);
+        $validUrl = $this->validateUrl($url);
         if ($validUrl !== true) return false;
         
         try {
-            $content = file_get_contents($imageUrl);
+            $content = file_get_contents($url);
             if ($content === false) {
-                $this->messages[] = "Unable to resize image";
-                $this->errorData[] = "Unknown image-url";
+                $this->setMessages("Unable to resize image");
+                $this->setErrorData("Unknown image-url");
                 return false;
             } else {
-                return $this->resizeByContent($content, $maxHeight, $maxWidth);
+                return $this->resize(null, $content, $maxHeight, $maxWidth, $thumbMaxHeight, $thumbMaxWidth);
             }
         } catch (\Throwable $e) {
-            $this->messages[] = "Unable to resize image";
-            $this->errorData[] = $e->getMessage();
+            $this->setMessages("Unable to resize image");
+            $this->setErrorData($e->getMessage());
             return false;
         }
     }
 
-    public function validateUrl($imageUrl)
+    /**
+     * Validate URL
+     *
+     * @param string $url
+     * @return boolean
+     */
+    public function validateUrl($url)
     {
-        $headers = get_headers($imageUrl);
+        $headers = get_headers($url);
         $statusCode = substr($headers[0], 9, 3);
         if ($statusCode != "200") {
-            $this->messages[] = "Invalid image-url (status-code: {$statusCode})";
+            $this->setMessages("Invalid image-url (status-code: {$statusCode})");
             return false;
         } else {
             return true;
         }
     }
 
+    /**
+     * Optimize image
+     *
+     * @param string $imageString
+     * @return boolean|string
+     */
     private function optimize($imageString)
     {
         // Create temporary file
@@ -124,9 +116,61 @@ class Image
             unlink($filename);
 
             // Set errors
-            $this->messages[] = "Unable to optimize image";
-            $this->errorData[] = $e->getMessage();
+            $this->setMessages("Unable to optimize image");
+            $this->setErrorData($e->getMessage());
             return false;
         }
     }
+
+    /**
+     * Resize image
+     *
+     * @param string|null $path
+     * @param string|null $content
+     * @param int $maxHeight
+     * @param int $maxWidth
+     * @param int|null $thumbMaxHeight
+     * @param int|null $thumbMaxWidth
+     * @return array|boolean|string
+     */
+    private function resize($path = null, $content = null, $maxHeight, $maxWidth, $thumbMaxHeight = null, $thumbMaxWidth = null)
+    {
+        if (empty($path) && empty($content)) {
+            $this->setMessages("No image provided");
+            return false;
+        }
+
+        try {
+            // Initialize image
+            if (!empty($path)) $image = new ImageResize($path);
+            else $image = ImageResize::createFromString($content);
+            $thumb = (!empty($thumbMaxHeight) && !empty($thumbMaxWidth)) ? true : false;
+
+            // Resize image
+            $image->resizeToBestFit($maxWidth, $maxHeight);
+            $resizedImageString = $image->getImageAsString();
+
+            // Resize thumbnail (if requested)
+            if ($thumb === true) {
+                $image->resizeToBestFit($thumbMaxWidth, $thumbMaxHeight);
+                $resizedThumbString = $image->getImageAsString();
+            }
+
+            // Optimize image
+            $optimizedImageString = $this->optimize($resizedImageString);
+
+            // Optimize thumbnail (if requested)
+            if ($thumb === true) {
+                $optimizedThumbString = $this->optimize($resizedThumbString);
+            }
+
+            // Return
+            return ($thumb === true) ? ["image"=>$optimizedImageString, "thumbnail"=>$optimizedThumbString] : $optimizedImageString;
+        } catch (\Throwable $e) {
+            $this->setMessages("Unable to resize image");
+            $this->setErrorData($e->getMessage());
+            return false;
+        }
+    }
+
 }
