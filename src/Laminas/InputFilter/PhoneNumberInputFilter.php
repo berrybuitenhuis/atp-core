1e6e6e132635d9719a46723795bc4c54b8fa60a7
B. Buitenhuis <b.buitenhuis@bva-auctions.com>
2021-02-12 16:07:55 +0100

    Added phone-number input-filter

---
+++ src/Laminas/InputFilter/PhoneNumberInputFilter.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AtpCore\Laminas\InputFilter;
+
+use Laminas\InputFilter\InputFilter;
+
+class PhoneNumberInputFilter
+{
+
+    /**
+     * Get InputFilter for a (dutch) phone-number-type field
+     *
+     * @param $name
+     * @param bool $required
+     * @param array $allowedTypes
+     * @param string $countryCode
+     * @return InputFilter
+     */
+    public static function getFilter($name, $required = false, $allowedTypes = null, $countryCode = "nl")
+    {
+        if ($name == null) {
+            return null;
+        } else {
+            $filter = [
+                'name' => $name,
+                'required' => $required,
+                'filters' => [],
+                'validators' => [
+                    [
+                        'name' => 'PhoneNumber',
+                        'options' => [
+                            'country' => $countryCode,
+                            'allowed_types' => (!empty($allowedTypes)) ? $allowedTypes : null,
+                        ],
+                    ],
+                ],
+            ];
+
+            $inputFilter = new InputFilter();
+            $inputFilter->add($filter, $name);
+            return $inputFilter;
+        }
+    }
+
+}
