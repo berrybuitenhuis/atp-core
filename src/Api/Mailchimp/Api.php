378817c44495481c7f0f8d863394a7cab3d3fa23
berrybuitenhuis <berrybuitenhuis@gmail.com>
2021-11-12 14:25:40 +0100

    Upgrade packages Added Mailchimp API

---
+++ src/Api/Mailchimp/Api.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * API-information: https://mailchimp.com/developer/marketing/docs/fundamentals/
+ */
+namespace AtpCore\Api\Mailchimp;
+
+use AtpCore\BaseClass;
+use MailchimpMarketing\ApiClient;
+
+class Api extends BaseClass
+{
+
+    private $client;
+
+    /**
+     * Constructor
+     *
+     * @param string $dc
+     * @param string $apiKey
+     * @param boolean $debug
+     */
+    public function __construct($dc, $apiKey, $debug = false)
+    {
+        // Set client
+        $this->client = new ApiClient();
+        $this->client->setConfig([
+            'apiKey' => $apiKey,
+            'server' => $dc,
+        ]);
+
+        // Set debug
+        if ($debug) $this->client->setDebug($debug);
+
+        // Reset error-messages
+        $this->resetErrors();
+    }
+
+    /**
+     * Update member-tag in list
+     *
+     * @param string $emailAddress
+     * @param string $listId
+     * @param string $name
+     * @param string $status
+     * @return bool
+     */
+    public function updateMemberTag($emailAddress, $listId, $name, $status)
+    {
+        // Set member-tag (body)
+        $body = [
+            "tags" => [
+                [
+                    "name" => $name,
+                    "status" => $status
+                ],
+            ],
+            "is_syncing" => false
+        ];
+
+        // Update member-tag
+        try {
+            $this->client->lists->updateListMemberTags($listId, $this->getSubscriberHash($emailAddress), $body);
+        } catch (\Exception $ex) {
+            $this->setErrorData($ex->getTrace());
+            $this->setMessages($ex->getCode() . ": " . $ex->getMessage());
+            return false;
+        }
+
+        // Return
+        return true;
+    }
+
+    /**
+     * Generate subscriber-hash by email-address
+     *
+     * @param string $emailAddress
+     * @return string
+     */
+    private function getSubscriberHash($emailAddress)
+    {
+        return md5(strtolower($emailAddress));
+    }
+}
