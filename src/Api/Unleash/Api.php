1e86e580ad2f8c74bc37908c1d90b8bc04bbb12a
berrybuitenhuis <berrybuitenhuis@gmail.com>
2022-08-12 16:58:01 +0200

    Added feature-flag client (Unleash)

---
+++ src/Api/Unleash/Api.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * API-information:
+ * https://docs.getunleash.io/sdks/php_sdk
+ * https://github.com/Unleash/unleash-client-php
+ */
+namespace AtpCore\Api\Unleash;
+
+use AtpCore\BaseClass;
+use Cache\Adapter\Filesystem\FilesystemCachePool;
+use League\Flysystem\Adapter\Local;
+use League\Flysystem\Filesystem;
+use Unleash\Client\Unleash;
+use Unleash\Client\UnleashBuilder;
+use Unleash\Client\Configuration\UnleashContext;
+
+class Api extends BaseClass
+{
+    private Unleash $client;
+    private UnleashContext $context;
+
+    /**
+     * Constructor
+     *
+     * @param string $appUrl
+     * @param string $instanceId
+     * @param string $environment
+     * @param boolean $debug
+     */
+    public function __construct($appUrl, $instanceId, $environment, $cacheTTL = 600, $debug = false)
+    {
+        // Set client
+        $this->client = UnleashBuilder::createForGitlab()
+            ->withInstanceId($instanceId)
+            ->withAppUrl($appUrl)
+            ->withGitlabEnvironment($environment)
+            ->withCacheHandler(
+                new FilesystemCachePool(
+                    new Filesystem(
+                        new Local(sys_get_temp_dir()),
+                    ),
+                )
+            )
+            ->withCacheTimeToLive($cacheTTL)
+            ->build();
+
+        // Reset error-messages
+        $this->resetErrors();
+    }
+
+    public function setContext($userId, $ipAddress, $sessionId)
+    {
+        $this->context = new UnleashContext($userId, $ipAddress, $sessionId);
+    }
+
+    public function checkFeature($featureName, $defaulValue = false)
+    {
+        return $this->client->isEnabled($featureName, $this->context, $defaulValue);
+    }
+}
