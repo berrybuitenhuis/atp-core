760065bb2f15b2878ef28287d0d2645c93eeee69
berrybuitenhuis <berrybuitenhuis@gmail.com>
2024-01-25 20:07:49 +0100

    Added sending push-notification to users (instead of devices)

--- src/Communication/PushNotification.php
+++ src/Communication/PushNotification.php
@@ -36,7 +36,7 @@ class PushNotification extends BaseClass
      * @return bool
      * @throws Exception
      */
-    public function send($platform, $tokens, $message, $options = NULL)
+    public function sendToDevice($platform, $tokens, $message, $options = NULL)
     {
         if (strtolower($platform) == 'onesignal') {
             // Set receiver (overwrite from config)
@@ -54,6 +54,54 @@ class PushNotification extends BaseClass
             $notificationFields = [
                 'app_id' => $this->config['oneSignal']['appId'],
                 'include_player_ids' => $tokens,
+                'data' => $options,
+                'contents' => ["en"=>$message, "nl"=>$message]
+            ];
+            $notification = new Notification($notificationFields);
+
+            // Send notification
+            $result = $client->send($notification);
+            if ($result === false) {
+                $this->setErrorData($client->getErrorData());
+                $this->setMessages($client->getMessages());
+            }
+
+            // Return
+            return $result;
+        } else {
+            $this->addMessage("Unknown platform");
+            return false;
+        }
+    }
+
+    /**
+     * Send push-notification to alias(es) (or user(s))
+     *
+     * @param string $platform
+     * @param string|array $aliases
+     * @param string $message
+     * @param array $options
+     * @return bool
+     * @throws Exception
+     */
+    public function sendToAlias($platform, $aliases, $message, $options = NULL)
+    {
+        if (strtolower($platform) == 'onesignal') {
+            // Set receiver (overwrite from config)
+            if (!empty($this->config['oneSignal']['default_to'])) {
+                $aliases = [$this->config['oneSignal']['default_to']];
+            }
+
+            // Convert alias to array
+            if (!is_array($aliases) && is_string($aliases)) $aliases = [$aliases];
+
+            // Initialize client
+            $client = new Api($this->config['oneSignal']['host'], $this->config['oneSignal']['apiKey']);
+
+            // Setup notification
+            $notificationFields = [
+                'app_id' => $this->config['oneSignal']['appId'],
+                'include_aliases' => $aliases,
                 'data' => $options,
                 'contents' => ["en"=>$message, "nl"=>$message]
             ];
