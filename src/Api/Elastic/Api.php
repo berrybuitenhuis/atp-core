ef5fdd2c5bec905b9f0ff4766ae10612d88915bf
berrybuitenhuis <berrybuitenhuis@gmail.com>
2024-03-15 13:36:38 +0100

    Fixed Elastic authentication

--- src/Api/Elastic/Api.php
+++ src/Api/Elastic/Api.php
@@ -26,7 +26,7 @@ class Api extends BaseClass
         // Set client
         $this->client = ClientBuilder::create()
             ->setElasticCloudId($cloudId)
-            ->setApiKey($apiId, $apiKey)
+            ->setApiKey($apiKey, $apiId)
             ->build();
 
         // Reset error-messages
