a18db3617d6c62703ce850eea4a0b84480b5f8d1
berrybuitenhuis <berrybuitenhuis@gmail.com>
2024-12-20 14:10:07 +0100

    Added vehicle VIN-validator

--- src/Vehicle.php
+++ src/Vehicle.php
@@ -5,7 +5,7 @@ namespace AtpCore;
 class Vehicle
 {
     /**
-     * Convert power (kW) into horse-power
+     * Convert power (kW) into horsepower
      *
      * @param integer $power
      * @return string
@@ -19,5 +19,23 @@ class Vehicle
         } else {
             return 0;
         }
+    }
+
+    /**
+     * Validate Vehicle Identification Number (VIN) format
+     *
+     * @param string $vin
+     * @param integer $year
+     * @return boolean
+     */
+    public static function isValidVin($vin, $year)
+    {
+        // Specific check for vehicles older than 1981
+        if ($year < 1981) {
+            return preg_match('/^[A-HJ-NPR-Z0-9]{11,16}$/', $vin);
+        }
+
+        // Return
+        return preg_match('/^[A-HJ-NPR-Z0-9]{17}$/', $vin);
     }
 }
