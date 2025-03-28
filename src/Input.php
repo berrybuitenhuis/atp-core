976a19fe8e4a3237100989a493d8db2ff076c5b1
berrybuitenhuis <berrybuitenhuis@gmail.com>
2025-03-28 14:12:38 +0100

    Added paginator parameter by request-object

--- src/Input.php
+++ src/Input.php
@@ -390,6 +390,7 @@ class Input
         $params->orderBy = (isset($params->orderBy)) ? json_decode($params->orderBy, true) : null;
         $params->limit = (isset($params->limit)) ? (int) $params->limit : null;
         $params->offset = (isset($params->page) && (int) $params->page > 1) ? ((int) $params->page - 1) * $params->limit : null;
+        $params->paginator = (!isset($params->paginator) || (isset($params->paginator) && ($params->paginator == 'true' || $params->paginator == 1))) ? true : false;
         $params->debug = (isset($params->debug) && ($params->debug == 'true' || $params->debug == 1)) ? true : false;
         $params->customRequestId = (isset($params->customRequestId) && !empty($params->customRequestId)) ? $params->customRequestId : null;
 
