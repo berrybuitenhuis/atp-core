f0a64acec129741198d30a968352df3c173ab02b
berrybuitenhuis <berrybuitenhuis@gmail.com>
2025-06-27 15:30:01 +0200

    Added Error-class

---
+++ src/Error.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace AtpCore;
+
+class Error
+{
+    public function __construct(
+        private mixed $data = null,
+        private array $messages = [],
+        private array $stackTrace = [],
+    ) {}
+
+    /**
+     * Add error-message
+     */
+    public function addMessage(string $message, array $backtrace): void
+    {
+        $this->messages[] = $message;
+        $this->stackTrace[] = [
+            "file" => $backtrace[0]['file'],
+            "class" => $backtrace[0]['class'],
+            "function" => $backtrace[0]['function'],
+            "line" => $backtrace[0]['line'],
+            "message" => $message,
+        ];
+    }
+
+    /**
+     * Get error-data
+     */
+    public function getData(): ?array
+    {
+        return $this->data;
+    }
+
+    /**
+     * Get error-messages
+     */
+    public function getMessages(): array
+    {
+        return $this->messages;
+    }
+
+    /**
+     * Get stack trace of error(s)
+     */
+    public function getStackTrace(): array
+    {
+        return $this->stackTrace;
+    }
+
+    /**
+     * Set error-data
+     */
+    public function setData($data): void
+    {
+        $this->data = $data;
+    }
+
+}
