8c8e27caca4aba95d7bf66225b753c26f9c4dc6c
berrybuitenhuis <berrybuitenhuis@gmail.com>
2026-01-28 20:05:46 +0100

    Fixed (non-)failing response mapping of JpCars-valuation due to extra/new property

--- src/Api/JpCars/Api.php
+++ src/Api/JpCars/Api.php
@@ -188,10 +188,10 @@ class Api extends BaseClass
                 return false;
             }
         } catch (\Exception $e) {
+            $this->setMessages($e->getMessage());
             if (stristr($e->getMessage(), "JSON property") && stristr($e->getMessage(), "does not exist in object of type AtpCore\Api\JpCars\Response\ValuateResponse")) {
                 return $this->mapResponse($response, new ValuateResponse(), false);
             }
-            $this->setMessages($e->getMessage());
             return false;
         }
 
