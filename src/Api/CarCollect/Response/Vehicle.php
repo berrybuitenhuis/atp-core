10e19c0ecaac137d05085cb4f907c0bdce1bfdda
berrybuitenhuis <berrybuitenhuis@gmail.com>
2026-09-04 09:27:25 +0200

    Undo: Improved model(name) API-mapping for CarCollect

--- src/Api/CarCollect/Response/Vehicle.php
+++ src/Api/CarCollect/Response/Vehicle.php
@@ -64,7 +64,7 @@ class Vehicle
     public $mileage_exact;
     /** @var integer|null */
     public $mileage_expected;
-    /** @var mixed */
+    /** @var string */
     public $model;
     /** @var integer|null */
     public $nap_check;
